27ef017a21cfcc027af43870cd4c935cbf9ce7cd
ojy <ojy9406@naver.com>
2022-08-18 19:07:49 +0900

    typeOrm test

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,40 +1,9 @@
 import { Body, Controller, Delete, Get, Param, Patch, Post, UsePipes, ValidationPipe } from '@nestjs/common'
 import { BoardsService } from './boards.service'
-import { Board, BoardStatus } from './board.model'
+import { BoardStatus } from './board.status.enum'
 import { CreateBoardDto } from './dto/create-board.dto'
 import { BoardStatusValidationPipe } from './pipes/board-status-validation.pipe'
 
 @Controller('boards')
 export class BoardsController {
-  constructor(private boardsSerivce: BoardsService) {}
-
-  @Get() 
-  getAllBoard(): Board[] {
-    return this.boardsSerivce.getAllBoards()
-  }
-
-  @Post()
-  @UsePipes(ValidationPipe)
-  createBoard(
-    @Body() createdBoardDto: CreateBoardDto
-  ): Board {
-    return this.boardsSerivce.createBoard(createdBoardDto)
-  }
-
-  @Get('/:id')
-  getBoardById(@Param('id') id: string): Board {
-    return this.boardsSerivce.getBoardById(id)
-  }
-
-  @Delete('/:id')
-  deleteBoard(@Param('id') id: string): void {
-    this.boardsSerivce.deleteBoard(id)
-  }
-
-  @Patch('/:id/status')
-  updateBoardStatus(
-    @Param('id') id: string, 
-    @Body('status', BoardStatusValidationPipe) status: BoardStatus) {
-    return this.boardsSerivce.updateBoardStatus(id, status)
-  }
 }
